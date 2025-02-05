4f57b36045283ea2749c492825b03047e6c05af4
anbumariyadass <anbumariyadass@gmail.com>
2025-02-05 13:22:27 +0530

    My First Java Program Commit

---
+++ SimpleProject/src/com/simple/SimpleFirstClass.java
@@ -0,0 +1,10 @@
+package com.simple;
+
+
+public class SimpleFirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is my first simple java class at Github");
+	}
+
+}
---
+++ SimpleProject/src/module-info.java
@@ -0,0 +1,2 @@
+module SimpleProject {
+}
